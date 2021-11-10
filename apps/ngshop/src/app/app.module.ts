436db481f56510ef8e22f9650b1a54361360ab27
i-salama <ibrah.salama@outlook.com>
2021-11-10 17:20:53 +0200

    Before admin panel

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -1,12 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { UiModule } from '@bluebits/ui';
 
+import {AccordionModule} from 'primeng/accordion';
+
+
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'products', component: ProductListComponent },
+
+]
+console.log(UiModule)
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent],
+  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule,BrowserAnimationsModule,AccordionModule],
+providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
