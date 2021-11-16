c16f5e86f081fc2affdb9b3fdd6b07bbe8c7243d
i-salama <ibrah.salama@outlook.com>
2021-11-16 23:26:21 +0200

    before migrating to windows

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,25 +1,47 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import {TableModule} from 'primeng/table';
+
 
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { CategoriesListComponent } from './categories/categories-list/categories-list.component';
+import { CategoriesFormComponent } from './categories/categories-form/categories-form.component';
+
+const UX_MODULES = [
+  CardModule,
+  ToolbarModule,
+  ButtonModule,
+  TableModule
+]
 
 const routes: Routes = [
-  { 
+  {
     path: "",
     component: ShellComponent,
-    children: [ 
-      {path: "dashboard", component:DashboardComponent}
-    ]},
+    children: [
+      { path: "dashboard", component: DashboardComponent },
+      { path: "categories", component: CategoriesListComponent },
+      { path: "categories/form", component: CategoriesFormComponent },
+
+    ]
+  },
 ]
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
+  declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent, CategoriesListComponent, CategoriesFormComponent],
   imports: [
     BrowserModule,
+    ...UX_MODULES,
+    HttpClientModule,
     RouterModule.forRoot(routes, { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
